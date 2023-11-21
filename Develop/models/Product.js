244389d2a55b327470937575b1cad284c64a83ca
romeo <rdumlao07@gmail.com>
2023-11-20 19:34:34 -0500

    product.init product_name column

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -15,7 +15,13 @@ Product.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     }
+
     
   },
   {
